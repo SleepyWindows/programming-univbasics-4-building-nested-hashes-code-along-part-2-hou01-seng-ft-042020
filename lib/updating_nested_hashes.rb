d33cca062699179104555f62c16ec88dea0d058d
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-12 00:06:17 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -51,7 +51,7 @@ def add_characters
          ]
       }
       :additional_characters => [
-        {:name => "Prince Escalus"}
+        {:name => "Prince Escalus"},
         {:name => "Apothecary"}
       ]
    }
